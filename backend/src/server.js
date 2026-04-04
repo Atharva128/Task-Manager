a0bebda633d716c3ea2e174a918a359305e7fa2d
Atharva Upale <76991339+Atharva128@users.noreply.github.com>
2026-04-04 14:48:39 +0530

    Fix server listening on hardcoded port

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,6 @@ const app = require('./app');
 
 const PORT = 3000;
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+app.listen(3000, '0.0.0.0' () => {
+  console.log(`Server running on port 3000`);
 });
