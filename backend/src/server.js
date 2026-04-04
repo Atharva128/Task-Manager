f02441af8f89879f5709ba817b280fd6a9d6c707
Atharva Upale <76991339+Atharva128@users.noreply.github.com>
2026-04-04 14:34:58 +0530

    Set default port to 3000 in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const app = require('./app');
 
+const PORT = 3000;
+
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
